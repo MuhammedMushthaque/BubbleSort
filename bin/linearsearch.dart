31d020824133b68e0507ef3b519e1c43d548d240
MuhammedMushthaque <mushthquekpp682@gmail.com>
2021-02-01 15:33:10 +0530

    linear search

--- bin/linearsearch.dart
+++ bin/linearsearch.dart
@@ -1,3 +1,14 @@
 void main() {
-  print('Hello world!');
+  List<num> numbers = [12, 34, 2, 5, 28];
+  int swap;
+  for (int i = 0; i < numbers.length; i++) {
+    for (int j = 1; j < numbers.length; j++) {
+      if (numbers[j] < numbers[j - 1]) {
+        swap = numbers[j];
+        numbers[j] = numbers[j - 1];
+        numbers[j - 1] = swap;
+      }
+    }
+  }
+  print(numbers);
 }
